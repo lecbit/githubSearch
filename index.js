a236a5f415f41cfcbfd6804adedda62d2e3030f6
lecbit <artm-marych@rambler.ru>
2020-07-29 13:24:22 +0200

    addFetch

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
+class View {
+  constructor(){
+    this.app = document.getElementById('app');
+    
+    this.title = this.createElement('h1', 'title');
+    this.title.textContent = 'Github Search Users';
+    
+    this.searchLine = this.createElement('div', 'search-line');
+    this.searchInput = this.createElement('input', 'search-input');
+    this.searchCounter = this.createElement('span', 'counter');
+    this.searchLine.append(this.searchInput);
+    this.searchLine.append(this.searchCounter);
+    
+    this.userWrapper = this.createElement('div', 'user-wrapper');
+    this.userList = this.createElement('ul', 'users');
+    this.userWrapper.append(this.userList);
+    
+    this.main = this.createElement('div', 'main');
+    this.main.append(this.userWrapper);
+    
+    this.app.append(this.title);
+    this.app.append(this.searchLine);
+    this.app.append(this.main);
+  }
+  createElement(elementTag, elementClass) {
+    const element = document.createElement(elementTag);
+    if (elementClass) {
+      element.classList.add(elementClass);
+    }
+    return element;
+  }
+}
 class Search{
-    constructor(){
-        this.app = document.getElementById('app');
-
-        this.title = this.createElement('h1', 'title');
-        this.title.textContent = 'Github Search Users';
-
-        this.searchLine = this.createElement('div','search-line');
-        this.searchInput = this.createElement('input','search-input');
-        this.searchCounter = this.createElement('span', 'counter');
-        this.searchLine.append(this.searchInput);
-        this.searchLine.append(this.searchCounter);
-
-        this.userWrapper = this.createElement('div', 'user-wrapper');
-        this.userList = this.createElement('ul', 'users');
-        this.userWrapper.append(this.userList);
-        
-        this.main = this.createElement('div', 'main');
-        this.main.append(this.userWrapper);
-
-        this.app.append(this.title);
-        this.app.append(this.searchLine);
-        this.app.append(this.main);
+    constructor(view){
+        this.view = view;
+        this.view.searchInput.addEventListener('keyup',this.searchUsers());
     }
 
-    createElement(elementTag, elementClass){
-        const element = document.createElement(elementTag);
-        if(elementClass){
-            element.classList.add(elementClass);
-        }
-        return element;
+    async searchUsers(){
+      return await fetch(``)
     }
 }
 
-const search = new Search();
+const search = new Search(new View());
