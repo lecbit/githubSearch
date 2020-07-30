97a254c0541f2ce0f026300c8eaebcf25994cffe
unknown <artm-marych@rambler.ru>
2020-07-30 21:09:07 +0200

    addModuleApi

---
+++ modules/api.js
@@ -0,0 +1,8 @@
+const URL = "https://api.github.com/";
+const USER_PER_PAGE = 20;
+
+class Api{
+    async loadUsers(value, page){
+        return await fetch(`${URL}search/users?q=${value}&per_page=${USER_PER_PAGE}&page=${page}`)
+    }
+}
